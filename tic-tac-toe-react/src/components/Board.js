fe43b84c00ed036024924a4b9af920d2120d66f5
Kimberly Salas <salask0104@gmail.com>
2023-06-30 10:35:57 -0700

    added css styles to board and made it a 3 by 3 grid

--- tic-tac-toe-react/src/components/Board.js
+++ tic-tac-toe-react/src/components/Board.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import { Box } from './components/Box';
+import "./Board.css"
 
 export const Board = ({ board, onclick }) => {
     return (
-        <div>
+        <div className='board'>
             {board.map((value, idx) => {
                 return <Box value={value} onclick={() => onclick(idx)} />
             })}
